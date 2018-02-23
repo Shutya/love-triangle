76524667d90836e19ffcae9d07f2393fc50695da
Dzianis Shutikov <Dzianis_Shutikov@epam.com>
2018-02-23 13:16:14 +0300

    tmp

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let sum = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    const conditionOne = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+    const conditionTwo = i + 1 !== preferences[i];
+    if( conditionOne && conditionTwo ) {
+      sum++;
+    }
+  }
+  return sum / 3;
 };
